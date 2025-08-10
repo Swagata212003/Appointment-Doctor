aaa6c7f28444644c36914289afc37da8f13de2ed
Swagata@212003 <Swagataroy594@gmail.com>
2025-08-10 09:01:20 +0530

    3rd  commit

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,24 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(
-//       `${process.env.MONGODB_CONNECTION_STRING}/prescripto`
-//     );
-
-//     console.log(
-//       `\nMongoDb connected || DB HOST:${connectionInstance.connection.host}`
-//     );
-//   } catch (error) {
-//     console.log("MONGODB connection FAILED ", error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
-
 
 import mongoose from "mongoose";
 
